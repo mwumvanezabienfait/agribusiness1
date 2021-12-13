c8d4e0e8d781245945647c20da32b1ea3410f9d6
mwumvanezabienfait <mwumvanezab@gmail.com>
2021-12-13 08:38:50 -0800

    'changes'

--- ussd/views.py
+++ ussd/views.py
@@ -8,7 +8,7 @@ def  welcome(request):
 
 #  python3 -m pip install africastalking
 AfricasUsername='mwumvanezab@gmail.com'
-api_key ='404d1c013a4de944b0a3553a13a31c1722d3a61b1a70f8356319a1d101a099a2'
+api_key ='9cd32813a0d258ce4fceded4ef097e84c0a878fbc3a883bf68d9149970a119df'
 africastalking.initialize(AfricasUsername,api_key)
 
 @csrf_exempt
@@ -25,47 +25,86 @@ def ussdApp(request):
         response =""
         #  main menu for our application
         if text == '':
-            response =  "CON Murakaza neza kuri Idafarm \n"
-            response += "1. Kwandikisha igihingwa \n"
-            response += "2. Kumenya ingengabihe \n"
+            response ="CON Murakoze murisanga kuri agribusiness \n"
+            response += "1. Kwandikisha ubutaka \n"
+            response += "2. Kwandikisha igihingwa \n"
+            response += "3. Kwandikisha umusaruro \n"
         elif text == '1':
-
-            response = "CON Hitamo igihingwa \n"
-            response += "1. Ibinyomoro \n"
-            response += "2. Indimu"
+            response = "CON Hitamo akarere \n"
+            response += "1.rubavu \n"
+            response += "2.nyabihu\n"
+            response += "3.musanze\n"
         elif text == '1*1':
-            product="Ibinyomoro"
-            response = "CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
-        elif category =='1*1' and int(len(level)) == 3 and str(level[2]) in  str(level):
-            response = "CON Uwo mubufatanyije \n"
-        elif category =='1*1' and int(len(level)) == 4 and str(level[3]) in  str(level):
-            response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
-        elif category =='1*1' and int(len(level)) == 5 and str(level[4]) in  str(level):
-            response = "END Murakoze kwiyandikisha kuri Ida farm \n"
+            response = "CON uzuza umwirondoro\n"
+            response += " numero yirangamuntu\n"
 
-
+        elif text == '1*1*1':
+            response = "CON Hitamo ubuso bwubutaka\n"
+            response += "1.hegitare 1-5 \n"
+            response += "2.hegitare 5-10\n"
+            response += "3.hegitare zirenze icumi\n"
+        elif text == '1*1*1*1':
+            response = "END Murakoze ,"
         elif text == '1*2':
-            product ="Indimu"
-            response ="CON shyiramo ubuso bw'ubutaka bwawe bw' "+str(product)+"\n"
-        elif category =='1*2' and int(len(level)) == 3 and str(level[2]) in  str(level):
-            response = "CON Uwo mubufatanyije \n"
-        elif category =='1*2' and int(len(level)) == 4 and str(level[3]) in  str(level):
-            response = "CON Shyiramo nimero y'irangamuntu yuwo mufatanyije \n"
-        elif category =='1*2' and int(len(level)) == 5 and str(level[4]) in  str(level):
-            response = "END Murakoze kwiyandikisha kuri Ida farm \n"
-         
-        #  ======================== INGENGABIHE==================
+            response = "CON uzuza umwirondoro\n"
+            response += "1.numero yirangamuntu\n"
+        elif text == '1*2*1':
+            response = "CON Hitamo ubuso bwubutaka\n"
+            response += "1.hegitare 1-5 \n"
+            response += "2.hegitare 5-10\n"
+            response += "3.hegitare zirenze icumi\n"
+        elif text == '1*3':
+            response = "CON uzuza umwirondoro\n"
+            response += "1.numero yirangamuntu\n"
+        elif text == '1*3*1':
+            response = "CON Hitamo ubuso bwubutaka\n"
+            response += "1.hegitare 1-5 \n"
+            response += "2.hegitare 5-10\n"
+            response += "3.hegitare zirenze icumi\n"
+
         elif text == '2':
-            response = "CON Hitamo igihe \n "
-            response += "1. Rimwe mukwezi \n"
-            response += "2. Kabiri Mukwezi \n"
-            response += "3. Buri gihe"
+            response = "CON shyiramo ubwoko bwigihingwa \n"
+            response += "1. ibirayi \n"
+            response += "2. ibishyimbo\n"
+            response += "3. ibigori\n"
         elif text == '2*1':
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe rimwe mukwezi"
+            response = "CON Hitamo ubwoko bwimbuto\n"
+            response += "1.kinigi \n"
+            response += "2.makoroni\n"
+        elif text == '2*1*1':
+            response = "END Murakoze "
+        elif text == '2*1*2':
+            response = "END Murakoze "
+
         elif text == '2*2':
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe kabiri mukwezi"
+            response = "CON Hitamo ubwoko bwimbuto\n"
+            response += "1.umutuku \n"
+            response += "2.umukara\n"
+        elif text == '2*2*1':
+            response = "END Murakoze "
+        elif text == '2*2*2':
+            response = "END Murakoze "
+
         elif text == '2*3':
-            response ="END Murakoze , tuzajya tubagezaho amakuru ku iteganyagihe Buri munsi"
+            response = "CON Hitamo ubwoko bwimbuto\n"
+            response += "1.tubura \n"
+            response += "2.inyarwanda\n"
+        elif text == '2*3*1':
+            response = "END Murakoze "
+        elif text == '2*3*2':
+            response = "END Murakoze "
+
+        elif text == '3':
+            response = "CON andika umusaruro\n"
+            response += "1. ibirayi \n"
+            response += "2. ibishyimbo\n"
+            response += "3. ibigori\n"
+        elif text == '3*1':
+            response = "END Murakoze kwandikisha umusaruro"
+        elif text == '3*2':
+            response = "END Murakoze kwandikisha umusaruro"
+        elif text == '3*3':
+            response = "END Murakoze kwandikisha umusaruro"
 
         else:
             response = "END Ukanze ibitaribyo, ongera mukanya"
